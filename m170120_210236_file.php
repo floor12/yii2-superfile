359cabfc5d424c1a48b99406f0f56e82aef801ca
floor12 <primelifecoder@gmail.com>
2017-01-20 22:04:19 +0100

    migration add

---
+++ m170120_210236_file.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m170120_210236_file extends Migration
+{
+
+    public function init()
+    {
+        $this->db = 'db';
+        parent::init();
+    }
+
+    public function safeUp()
+    {
+        $tableOptions = 'ENGINE=InnoDB';
+
+        $this->createTable(
+            '{{%file}}',
+            [
+                'id'=> $this->primaryKey(11),
+                'class'=> $this->string(255)->notNull(),
+                'field'=> $this->string(255)->notNull(),
+                'object_id'=> $this->integer(11)->notNull()->defaultValue(0),
+                'title'=> $this->string(255)->notNull(),
+                'filename'=> $this->string(255)->notNull(),
+                'content_type'=> $this->string(255)->notNull(),
+                'type'=> $this->integer(1)->notNull(),
+                'video_status'=> $this->integer(1)->null()->defaultValue(null),
+                'ordering'=> $this->integer(11)->notNull()->defaultValue(0),
+                'created'=> $this->integer(11)->notNull(),
+                'user_id'=> $this->integer(11)->notNull(),
+                'size'=> $this->bigint(20)->notNull(),
+            ],$tableOptions
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('{{%file}}');
+    }
+}
